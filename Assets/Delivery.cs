f9bd4921849f4e7057b5f5f20b12853bc276ac66
lordsayur <umar_317@hotmail.com>
2021-11-13 10:18:46 +0800

    ✨ 25. How To Destroy Objects

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -9,10 +9,11 @@ public class Delivery : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log(other.name + " picked up.");
             hasPackage = true;
+            Destroy(other.gameObject);
         }
 
         if (other.tag == "Customer" && hasPackage)
