6941247184b0b995045bfc99f0ee0d5b97cdf903
lordsayur <umar_317@hotmail.com>
2021-11-13 22:52:01 +0800

    ✨ Implement camera following player

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -1,10 +1,24 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    [SerializeField] GameObject target;
+    GameObject target;
+
+    private void Start()
+    {
+        var players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (var player in players)
+        {
+            if (PhotonView.Get(player).IsMine)
+            {
+                target = player;
+                break;
+            }
+        }
+    }
 
     void LateUpdate()
     {
