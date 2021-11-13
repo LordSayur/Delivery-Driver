a8f9cda7cef86779e5c519dc25797b6b0f023cb5
lordsayur <umar_317@hotmail.com>
2021-11-13 22:30:54 +0800

    ✨ Implement control specific player only

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class Driver : MonoBehaviour
 {
@@ -13,19 +14,23 @@ public class Driver : MonoBehaviour
     [SerializeField]
     float boostSpeed = 30f;
 
+    PhotonView photonView;
+
     void Start()
     {
-        
+        photonView = GetComponent<PhotonView>();
     }
 
     void Update()
     {
+        if (photonView.IsMine)
+        {
+            float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+            float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
-        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-
-        transform.Rotate(0, 0, -steerAmount);
-        transform.Translate(0, moveAmount, 0);
+            transform.Rotate(0, 0, -steerAmount);
+            transform.Translate(0, moveAmount, 0);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
